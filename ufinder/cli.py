f1b9204ebf9a0c2d076ffa46297fe7679e9738ef
claytod5 <claytod5@protonmail.com>
2021-12-20 15:28:25 -0500

    refactor: comment out argparse code

--- ufinder/cli.py
+++ ufinder/cli.py
@@ -4,61 +4,63 @@ import sys
 from ufinder import ufinder
 
 
-def main():
-
-    app = ufinder.App()
-
-    parser = argparse.ArgumentParser(
-        description="Find a user and their associated network equipment",
-        epilog="Please report issues to dclayton@bluegrasscellular.com",
-    )
-    subparsers = parser.add_subparsers(title="Commands", prog="ufinder", metavar="")
-
-    username = subparsers.add_parser(
-        "username",
-        usage="ufinder username [-h] [username]",
-        help="Find user by username",
-    )
-    username.add_argument("username", type=str, help="A username. (i.e. mjackson)")
-    username.set_defaults(func=app.get_by_username)
-
-    computer = subparsers.add_parser(
-        "computer",
-        usage="ufinder computer [-h] [computer_name]",
-        help="Find user by computer name",
-    )
-    computer.add_argument(
-        "computer_name", type=str, help="A computer name. (i.e.  ET1INF01)"
-    )
-    computer.set_defaults(func=app.get_by_machine)
-
-    args = parser.parse_args()
-
-    try:
-        print(args.func(args))
-    except AttributeError:
-        parser.print_help()
-
-    return 0
-
-# TODO: add ability to refresh data in DB
+# def main():
+# 
+#     app = ufinder.App()
+# 
+#     parser = argparse.ArgumentParser(
+#         description="Find a user and their associated network equipment",
+#         epilog="Please report issues to dclayton@bluegrasscellular.com",
+#     )
+#     subparsers = parser.add_subparsers(title="Commands", prog="ufinder", metavar="")
+# 
+#     username = subparsers.add_parser(
+#         "username",
+#         usage="ufinder username [-h] [username]",
+#         help="Find user by username",
+#     )
+#     username.add_argument("username", type=str, help="A username. (i.e. mjackson)")
+#     username.set_defaults(func=app.get_by_username)
+# 
+#     computer = subparsers.add_parser(
+#         "computer",
+#         usage="ufinder computer [-h] [computer_name]",
+#         help="Find user by computer name",
+#     )
+#     computer.add_argument(
+#         "computer_name", type=str, help="A computer name. (i.e.  ET1INF01)"
+#     )
+#     computer.set_defaults(func=app.get_by_machine)
+# 
+#     args = parser.parse_args()
+# 
+#     try:
+#         print(args.func(args))
+#     except AttributeError:
+#         parser.print_help()
+# 
+#     return 0
 
 def interactive_main():
 
     app = ufinder.App()
 
+    # TODO: make this happen the first time, but not afterwards
     print(
         """Welcome to ufinder!
 
-    Please report any issues to dclayton@bluegrasscellular.com"""
+    If this is the first launch of ufinder, you will want to initialize the database.
+    You can do this by selecting option 3.
+    Once that is complete, you can begin searching!"""
     )
 
     search_method_prompt = """
-    
+
     How would you like to search?
 
     [1] Username
     [2] Computer
+    [3] Refresh data in database
 
     [0] Exit
 
